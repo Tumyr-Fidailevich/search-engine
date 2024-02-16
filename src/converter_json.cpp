dd2b34c882afc79ed29954fd5ef9b601789e29cf
Timur <timurkashapov15@gmail.com>
2024-02-16 15:29:54 +0300

    add getTextDocuments implementation

--- src/converter_json.cpp
+++ src/converter_json.cpp
@@ -1,6 +1,46 @@
 #include "converter_json.h"
 
-void something()
+
+static const std::string resourcePath = "../../resources";
+static const std::string requestsPath = "../../config";
+
+
+std::vector<Document> ConverterJson::getTextDocuments() noexcept
 {
+    if(!std::filesystem::exists(resourcePath)) return {};
+
+    std::vector<Document> results;
+    for(const auto& file : std::filesystem::directory_iterator(resourcePath))
+    {
+        std::ifstream fileStream(file.path());
+        if(fileStream.is_open())
+        {
+            std::string documentValue;
+            fileStream >> documentValue;
+            results.emplace_back(file.path().filename(), std::move(documentValue));
+        }
+    }
+    return std::move(results);
+}
+
+std::vector<std::string> ConverterJson::getRequests()
+{
+    if(!std::filesystem::exists(requestsPath)) throw std::runtime_error("Requests file doesn't exists");
+
+    std::ifstream requestsFileStream(requestsPath);
+
+    if(!requestsFileStream.is_open()) throw std::runtime_error("Can't open requests file");
+
+    nlohmann::json requestsJson;
+
+    requestsFileStream >> requestsJson;
     
+    if(!requestsJson.contains("requests")) throw std::runtime_error("Requests file is empty");
+
+    return std::move(requestsJson["requests"]);
+}
+
+void ConverterJson::putAnswers(const std::vector<std::vector<std::pair<int, float>>>& answers)
+{
+
 }
