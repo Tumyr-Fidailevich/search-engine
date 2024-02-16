23de9686bda562094f4774f89469721f7a871c10
Timur <timurkashapov15@gmail.com>
2024-02-17 00:59:25 +0300

    add precompile headers

---
+++ include/pch.h
@@ -0,0 +1,15 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+#include <filesystem>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <stdexcept>
+#include "nlohmann/json.hpp"
+
+#endif // PCH_H
